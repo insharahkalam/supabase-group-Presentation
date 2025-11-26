b2adf84445058d91f15b06da74190a488ba9d159
insharahkalam <insharahkalam19@gmail.com>
2025-11-27 02:16:13 +0500

    supabase added

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
-const projectUrl = "https://dnehwweprdbyseuiouie.supabase.co"
-const projectKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuZWh3d2VwcmRieXNldWlvdWllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExNTA5NTgsImV4cCI6MjA3NjcyNjk1OH0.i49X5PJFTzyL5LRyxaQykGUEmOtoeYOslMXQs4Oegug"
+const projectUrl = "https://jzxxnlisarhqbdkqobmy.supabase.co"
+const projectKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6eHhubGlzYXJocWJka3FvYm15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyNDEyMzQsImV4cCI6MjA3NjgxNzIzNH0.blYwoj2ObZ0Om5UoARD07HNFk9xXvfKmXIfihRIzf8Q"
 const { createClient } = supabase;
-const client = createClient(projectUrl,projectKey)
+const client = createClient(projectUrl, projectKey)
 
 console.log(createClient);
 console.log(client);
+
+// =======inserting form data=======
+
+const title = document.getElementById("title")
+const description = document.getElementById("description")
+const issue_type = document.getElementById("issue_type")
+const priority = document.getElementsByName("priority")
+const submitBtn = document.getElementById("submitBtn")
+let selectedPriority = "";
+submitBtn.addEventListener("click", async (e) => {
+    e.preventDefault()
+    for (let i = 0; i < priority.length; i++) {
+        console.log(priority[i].checked);
+        if (priority[i].checked) {
+            selectedPriority = priority[i].value;
+            break;
+        }
+    }
+
+    const { error } = await client
+        .from('Ticket_form')
+        .insert({ Title: title.value, Description: description.value, Priority: selectedPriority, Issue_type: issue_type.value })
+
+    if (error) {
+        console.log("data inserting error", error.message);
+    } else {
+        alert("data insert successfully!!")
+    }
+
+
+
+})
+
+
