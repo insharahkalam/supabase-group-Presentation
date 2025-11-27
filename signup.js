f0a9c475f18df6a3e6f3c99d4fbd7ced29bb2ecd
khola azeem <kholaazeem104@gmail.com>
2025-11-27 19:53:44 +0500

    UI updation done

--- signup.js
+++ signup.js
@@ -1,11 +1,30 @@
 import supaBase from "./config.js";
 
 
-let sUName = document.getElementById("UserName");
+let sUName = document.getElementById("name");
 let sEmail = document.getElementById ("email");
 let sPass = document.getElementById ("password");
-let sPhn = document.getElementById ("phoneNo");
-let sBtn = document.querySelector ("#login-btn input");
+let sPhn = document.getElementById ("ph-no.");
+let sBtn = document.querySelector (".btn-signup");
+
+
+    //  pasword toggle button
+    
+ const togglePass = document.querySelector(".toggle-password")
+ 
+ togglePass.addEventListener("click", ()=>{
+   if (sPass.type === "password"){
+    sPass.type = "text"
+    togglePass.classList.remove("fa-eye-slash")
+     togglePass.classList.add("fa-eye")
+   }else{
+    sPass.type = "password"
+    togglePass.classList.remove("fa-eye")
+     togglePass.classList.add("fa-eye-slash")
+   }
+ })
+
+ 
          
 
         //  SIGN UP FUNCTIONALITY
@@ -20,8 +39,18 @@ async function signUp(e){
         Swal.fire({
             title: "All fields required!",
             text: "Please fill all fields before signup.",
-            icon: "warning"
+            icon: "warning",
+            background: "#f9fbfc",
+            color: "#003b46",
+            confirmButtonColor:  "#003b46",
+              confirmButtonText: "OK",
+            padding: "20px",
+            borderRadius:"15px",
+                  customClass: {
+    popup: "glass-alert"
+  }
         })
+
      return
     };
     
@@ -30,8 +59,16 @@ if (sPhn.value.length !== 11) {
     Swal.fire({
         title: "Incorrect Phone Number!",
         text: "Phone number must be exactly 11 digits.",
-        icon: "warning"
-    });
+        icon: "warning",
+        background: "#f9fbfc",
+        color: "#003b46",
+        confirmButtonColor: "#003b46",
+           confirmButtonText: "Try Again",
+            customClass: {
+    popup: "glass-alert"
+  }
+ 
+    })
     return;
 }
 
@@ -59,15 +96,37 @@ if (sPhn.value.length !== 11) {
   title: "Signup Failed!" ,
   text: error.message,
   icon: "error",
-  draggable: true
+  draggable: true,
+  background: "#f9fbfc",
+  color: "#003b46",
+  confirmButtonColor:  "#003b46",
+  confirmButtonText: "OK",
+  padding: "20px",
+ borderRadius:"15px",
+       customClass: {
+    popup: "glass-alert"
+  }
 })
 return;
+
   }else {
    Swal.fire({
   title: "Signup successfully!",
+  text: "Welcome to Ticket Manager",
   icon: "success",
-  draggable: true
-}).then (()=>{
+  draggable: true,
+  background: "#f9fbfc",
+  color: "#003b46",
+  confirmButtonColor:  "#003b46",
+  confirmButtonText: "Go to Home",
+  padding: "20px",
+ borderRadius:"15px",
+       customClass: {
+    popup: "glass-alert"
+  }
+ 
+})
+.then (()=>{
   location.href = "home.html"
 })
  
