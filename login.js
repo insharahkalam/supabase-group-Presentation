ea9927d17cd1df6127bd83d21b011f07239d2ad5
Azka Azeem <azkaazeem804@gmail.com>
2025-12-03 19:50:43 +0500

    add funcnality in forget & update password

--- login.js
+++ login.js
@@ -5,11 +5,12 @@ let lPass = document.getElementById("password");
 let lBtn = document.querySelector("#login-btn");
 
 
-//  pasword toggle button
+//  pasword toggle button for login and forget password page
 
 const togglePass = document.querySelector(".toggle-password")
-  function toggleIcon(){
- 
+
+function toggleIcon() {
+    if (!lPass) return;
     if (lPass.type === "password") {
         lPass.type = "text"
         togglePass.classList.remove("fa-eye-slash")
@@ -21,12 +22,16 @@ const togglePass = document.querySelector(".toggle-password")
     }
 }
 
- togglePass &&togglePass.addEventListener("click", toggleIcon)
+togglePass && togglePass.addEventListener("click", toggleIcon)
+
+
+
+//   LOGIN FUNCTIONALTY
 
 
 async function login(e) {
     e.preventDefault();
-   
+
     let email = lEmail.value.trim();
     let pass = lPass.value.trim();
 
@@ -216,7 +221,7 @@ async function login(e) {
             customClass: {
                 popup: "glass-alert"
             }
-        }).then ( () => {
+        }).then(() => {
             lEmail.value = "";
             lPass.value = "";
         })
@@ -228,32 +233,106 @@ lBtn && lBtn.addEventListener("click", login);
 
 
 
-                        //    FORGET PASSWORD FUNCINALITY
+//    FORGET PASSWORD FUNCINALITY
 
 
 const resetBtn = document.getElementById("resetBtn");
- const resEmail = document.getElementById("reset-email");
+const resEmail = document.getElementById("reset-email");
 
 
-async function reset(){
+async function reset(e) {
+    e.preventDefault();
 
     if (!resEmail.value) {
-        alert("Please enter an email");
-       return
+        console.log("Input is Empty!!");
+        Swal.fire({
+            title: "Email required!",
+            text: "Please Enter your Email.",
+            icon: "warning",
+            background: "#f9fbfc",
+            color: "#003b46",
+            confirmButtonColor: "#003b46",
+            confirmButtonText: "OK",
+            padding: "20px",
+            borderRadius: "15px",
+            customClass: {
+                popup: "glass-alert"
+            }
+        })
+        return;
     }
-   
-  
-    const { data, error } = await supaBase.auth.resetPasswordForEmail(resEmail.value, {
-        redirectTo: "https://azkaazeem.github.io/Login-page---Update-Password-page" 
-      
-    });
 
-    if (error) {
-        alert("Error: " + error.message);
-    } else {
-        alert("Reset link sent to your email!");
+    try {
+
+
+        const { data, error } = await supaBase.auth.resetPasswordForEmail(resEmail.value, {
+            redirectTo: "https://azkaazeem.github.io/Login-page---Update-Password-page"
+
+        });
+
+        if (error) {
+            console.log('Supabase Error:' + ' ' + error.message);
+            Swal.fire({
+                title: "Error!",
+                text: error.message,
+                icon: "error",
+                draggable: true,
+                background: "#f9fbfc",
+                color: "#003b46",
+                confirmButtonColor: "#003b46",
+                confirmButtonText: "OK",
+                padding: "20px",
+                borderRadius: "15px",
+                customClass: {
+                    popup: "glass-alert"
+                }
+            }).then(() => {
+                resEmail.value = "";
+            })
+
+
+        } else {
+            console.log('Reset link sent to you Email..')
+
+            Swal.fire({
+                title: "Success!",
+                text: "Reset link sent to you Email..",
+                icon: "success",
+                draggable: true,
+                // timer: 3000,
+                showConfirmButton: false,
+                background: "#f9fbfc",
+                color: "#003b46",
+                padding: "20px",
+                borderRadius: "15px",
+                customClass: {
+                    popup: "glass-alert"
+                }
+
+            })
+        }
+
+    } catch (err) {
+        console.log(err)
+        Swal.fire({
+            title: "System error!",
+            html: `Something went wrong internally! <br></br> <b>${err.message || "Unknown error"}</b>`,
+            icon: "error",
+            background: "#f9fbfc",
+            color: "#003b46",
+            confirmButtonColor: "#003b46",
+            confirmButtonText: "Report issue",
+            padding: "20px",
+            borderRadius: "15px",
+            customClass: {
+                popup: "glass-alert"
+            }
+        }).then(() => {
+            resEmail.value = "";
+        })
     }
-};
+}
+
 
 resetBtn && resetBtn.addEventListener("click", reset)
 
@@ -263,103 +342,161 @@ resetBtn && resetBtn.addEventListener("click", reset)
 
 
 
-// ADD UPDATE PASSWORD FUNCTIONALITY
+//      UPDATE PASSWORD FUNCTIONALITY
 
 let newPassInp = document.getElementById("newPass");
 let conPassInp = document.getElementById("confirmPass");
 let ubdBtn = document.getElementById("updatePassBtn");
 
-async function newPass() {
-try {
-    
 
+async function newPass(e) {
+    e.preventDefault();
 
-    const { data, error } = await supaBase.auth.updateU
-    
-    
-    ser({
-        password: newPassInp.value
-    });
+    console.log("Button is clicked!!!!");
 
-    if (error) {
-    console.log(error.message);
-    
-    }else{
+    if (!newPassInp.value && !conPassInp.value) {
+        console.log("Input is Empty!!");
         Swal.fire({
-                title: 'Success!',
-                text: 'Your password has been updated successfully. Redirecting to login...',
-                icon: 'success',
-                timer: 3000,
-                showConfirmButton: false
-            }).then(() => {
-                // User ko login page par bhej dein
-                location.href = 'https://azkaazeem.github.io/Login-page/'; // Isay apne login page ka URL dein
-            });
-    } 
+            title: "Password fields required!",
+            text: "Please Enter New password and Confirm password.",
+            icon: "warning",
+            background: "#f9fbfc",
+            color: "#003b46",
+            confirmButtonColor: "#003b46",
+            confirmButtonText: "OK",
+            padding: "20px",
+            borderRadius: "15px",
+            customClass: {
+                popup: "glass-alert"
+            }
+        })
 
-} catch (error) {
-    
+        return;
+    }
+
+    if (newPassInp.value !== conPassInp.value) {
+
+        console.log("Passwords are not equal!!");
+
+        Swal.fire({
+            title: "Password Do Not Match",
+            text: "The New Password and Confirm Password fields must be identical..",
+            icon: "warning",
+            background: "#f9fbfc",
+            color: "#003b46",
+            confirmButtonColor: "#003b46",
+            confirmButtonText: "OK",
+            padding: "20px",
+            borderRadius: "15px",
+            customClass: {
+                popup: "glass-alert"
+            }
+        })
+
+        return;
+    }
+
+
+    try {
+
+        const { data, error } = await supaBase.auth.updateUser({
+            password: newPassInp.value
+        });
+
+        if (error) {
+            console.log(error.message);
+            Swal.fire({
+                title: "Updation Failed!",
+                text: error.message,
+                icon: "error",
+                draggable: true,
+                background: "#f9fbfc",
+                color: "#003b46",
+                confirmButtonColor: "#003b46",
+                confirmButtonText: "OK",
+                padding: "20px",
+                borderRadius: "15px",
+                customClass: {
+                    popup: "glass-alert"
+                }
+            }).then(() => {
+                newPassInp.value = "";
+                conPassInp.value = "";
+            })
+
+
+        } else {
+            Swal.fire({
+                title: "Success!",
+                text: "Your password has been updated successfully. Redirecting to login.",
+                icon: "success",
+                draggable: true,
+                timer: 3000,
+                showConfirmButton: false,
+                background: "#f9fbfc",
+                color: "#003b46",
+                padding: "20px",
+                borderRadius: "15px",
+                customClass: {
+                    popup: "glass-alert"
+                }
+
+            })
+                .then(() => {
+                    location.href = 'https://azkaazeem.github.io/Login-page/';
+                });
+        }
+
+    } catch (err) {
+        console.log(err)
+        Swal.fire({
+            title: "System error!",
+            html: `Something went wrong internally! <br></br> <b>${err.message || "Unknown error"}</b>`,
+            icon: "error",
+            background: "#f9fbfc",
+            color: "#003b46",
+            confirmButtonColor: "#003b46",
+            confirmButtonText: "Report issue",
+            padding: "20px",
+            borderRadius: "15px",
+            customClass: {
+                popup: "glass-alert"
+            }
+        }).then(() => {
+            newPassInp.value = "";
+            conPassInp.value = "";
+
+        })
+    }
 }
 
-
-} 
-
-ubdBtn.addEventListener ("click", newPass)
+ubdBtn && ubdBtn.addEventListener("click", newPass)
 
 
+// SHOW/HIDE PASSWORD TOGGLE  for update password
+
+let toggles = document.querySelectorAll(".toggle-password");
+
+toggles.forEach(toggle => {
+
+    function toggIcon() {
+        let id = toggle.getAttribute("data-target");
+        let input = document.getElementById(id);
+
+        if (input.type === "password") {
+            input.type = "text";
+            toggle.classList.replace("fa-eye-slash", "fa-eye");
+        } else {
+            input.type = "password";
+            toggle.classList.replace("fa-eye", "fa-eye-slash");
+        }
+
+        console.log("Cutie Eye Icon is clicked!!!!");
 
 
+    }
 
+    toggle && toggle.addEventListener("click", toggIcon)
+}
 
-
-
-
-
-
-// SHOW/HIDE PASSWORD TOGGLE
-
-
-
-// let toggles = document.querySelectorAll(".toggle-password");
-
-// toggles.forEach(toggle => {
-//     function toggIcon() {
-//         let id = toggle.getAttribute("data-target");
-//         let input = document.getElementById(id);
-
-//         if (input.type === "password") {
-//             input.type = "text";
-//             toggle.classList.replace("fa-eye-slash", "fa-eye");
-//         } else {
-//             input.type = "password";
-//             toggle.classList.replace("fa-eye", "fa-eye-slash");
-//         }
-
-//     }
-
-//     toggle && toggle.addEventListener("click", toggIcon)
-// }
-
-// )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+)
