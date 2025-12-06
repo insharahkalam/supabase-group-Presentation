f6f48f47ef81cd7914a080d432061300d6670ac4
Afshan kanwal22 <kanwalafshan2244@gmail.com>
2025-12-06 10:44:35 +0500

    update

---
+++ app.js
@@ -0,0 +1,6 @@
+const projectUrl="https://dnehwweprdbyseuiouie.supabase.co"
+const projectKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuZWh3d2VwcmRieXNldWlvdWllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExNTA5NTgsImV4cCI6MjA3NjcyNjk1OH0.i49X5PJFTzyL5LRyxaQykGUEmOtoeYOslMXQs4Oegug"
+const { createClient }= supabase;
+const client =createClient(projectUrl,projectKey);
+console.log(client)
+console.log(createClient);
